bede3aa8c30615a4eb5c7066eb86f741c3322916
StoRm619 <matthewsabebe23@gmail.com>
2021-07-01 01:50:35 -0700

    Change Products from set to dynamic

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
-const products = [
-    { id: 1, name: 'Steak', description: '10oz Ribeye', price: '$20', image: 'https://embed.widencdn.net/img/beef/ng96sbyljl/800x600px/Ribeye%20Steak_Lip-on.psd?keep=c&u=7fueml' },
-    { id: 2, name: 'Ice Cream', description: 'Cookie and Cream', price: '$5', image: 'https://domesticallyblissful.com/wp-content/uploads/2020/07/Double-Stuffed-Oreo-Ice-Cream.jpg' }
-]
 
 
 
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
