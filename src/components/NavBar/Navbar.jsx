0188fcc0c9a7621eb2f04c1d730b92ef7653159c
StoRm619 <matthewsabebe23@gmail.com>
2021-07-02 00:21:54 -0700

    Cart Button and Cart Function added

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
-
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/images/Icon.png';
 import useStyles from './styles';
 
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
+
+    {
+
+    }
+
     return (
         <div>
             <AppBar position="fixed" className={classes.AppBar} color='inherit'>
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color='inherit'>
+                    <Typography component={Link} to='/' variant="h6" className={classes.title} color='inherit'>
                         <img src={logo} alt='Food Ecomm' height='25px' className={classes.image} />
                         Food Ecomm
                     </Typography>
                     <div className={classes.grow} />
-                    <div className={classes.buton}>
-                        <IconButton aria-label="Show cart items" color='inherit'>
+                    {location.pathname === '/' && (<div className={classes.button}>
+                        <IconButton component={Link} to='/cart' aria-label="Show cart items" color='inherit'>
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div>)}
+
                 </Toolbar>
             </AppBar>
 
